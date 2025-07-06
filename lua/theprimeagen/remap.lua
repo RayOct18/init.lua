37099bca667b7f325aeba0bb7652099b31e7fd13
rayxie <birken1018@gmail.com>
2025-07-06 23:07:00 +0800

    update

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -4,6 +4,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set('n', '<leader>us', ':bmodified<CR>', { noremap = true, silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
